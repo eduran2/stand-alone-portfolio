4b569a2561a8159bbe7b067ad65cd5a0d6a4cdfc
eduran2 <Eduardo@Eduardos-Mac.local>
2016-11-30 03:43:02 -0800

    fixing images for website

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
 // Store images
-var img0 = '/img/fullsize/asad.jpg';
-var img1 = '/img/fullsize/ewuframe.jpg';
-var img2 = '/img/fullsize/poachedsign.jpg';
-var img3 = '/img/fullsize/swcan.jpg';
-var img4 = '/img/fullsize/whiskeyad2.jpg';
+var img0 = '/stand-alone-portfolio/img/fullsize/asad.jpg';
+var img1 = '/stand-alone-portfolio/img/fullsize/ewuframe.jpg';
+var img2 = '/stand-alone-portfolio/img/fullsize/poachedsign.jpg';
+var img3 = '/stand-alone-portfolio/img/fullsize/swcan.jpg';
+var img4 = '/stand-alone-portfolio/img/fullsize/whiskeyad2.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );

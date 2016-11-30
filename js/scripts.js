c50a6dabd4c1568eba7dbdc3ee032e5fd6f16649
eduran2 <Eduardo@Eduardos-Mac.local>
2016-11-30 01:36:10 -0800

    added images and fixed sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 // Randomize Header
 
 // Store & Generate Number
-var maxRand = 3;
+var maxRand = 5;
 var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
 // Store images
-var img0 = '/img/fullsize/1.jpg';
-var img1 = '/img/fullsize/2.jpg';
-var img2 = '/img/fullsize/3.jpg';
+var img0 = '/img/fullsize/asad.jpg';
+var img1 = '/img/fullsize/ewuframe.jpg';
+var img2 = '/img/fullsize/poachedsign.jpg';
+var img3 = '/img/fullsize/swcan.jpg';
+var img4 = '/img/fullsize/whiskeyad2.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );
